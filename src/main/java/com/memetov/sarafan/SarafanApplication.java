5c847d73faf6855ed309cdd20b1ebca3138cdf4a
arsenmemetov99 <arsen.memetov.1999@gmai.com>
2019-09-17 15:00:43 +0300

    Initial commit

---
+++ src/main/java/com/memetov/sarafan/SarafanApplication.java
@@ -0,0 +1,13 @@
+package com.memetov.sarafan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarafanApplication.class, args);
+	}
+
+}
